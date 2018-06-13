f3980b038c512b7feac410fa71149bbe70114bd6
Joe Lane <lanejo01@luther.edu>
2018-06-13 14:36:55 -0500

    clean up defmethods

--- src/iontalk/responder.clj
+++ src/iontalk/responder.clj
@@ -50,36 +50,28 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn respond-dispatch
   [{:strs [To From Body] :as msg}]
-  (println "Got to respond-dispatch")
-  (println (pr-str msg))
   {:tags (set (hashtags Body))})
 
 (defmulti respond respond-dispatch)
 
 (defmethod respond {:tags #{"#apropos"}}
-  [context]
-  (println "got to apropos")
-  (println (pr-str context))
-  (let [db (get context "db" (d/db (ion/get-connection)))
-        Body (get context "Body")]
-    (->> (some-body Body)
-         (d/q
-          '[:find ?text ?author
-            :in $ ?search
-            :where
-            [?e :fortune/author ?author]
-            [?e :fortune/text ?text]
-            [(= ?apropos true)]
-            [(iontalk.responder/apropos? $ ?e ?search) ?apropos]]
-          db)
-         (map #(->> % (str/join " -")))
-         (str/join ", "))))
+  [{:strs [db Body] :as context}]
+  (->> (some-body Body)
+       (d/q
+        '[:find ?text ?author
+          :in $ ?search
+          :where
+          [?e :fortune/author ?author]
+          [?e :fortune/text ?text]
+          [(= ?apropos true)]
+          [(iontalk.responder/apropos? $ ?e ?search) ?apropos]]
+        db)
+       (map #(->> % (str/join " -")))
+       (str/join ", ")))
 
 
 (defmethod respond {:tags #{"#new-fortune"}}
   [context]
-  (println "got to new-fortune")
-  (println (pr-str context))
   (let [db (get context "db" (d/db (ion/get-connection)))]
     (d/q
      '[:find ?author (count ?text)
@@ -89,42 +81,34 @@
      db)))
 
 (defmethod respond {:tags #{"#fortune"}}
-  [context]
-  (println "got to fortune")
-  (println (pr-str context))
-  (let [db (get context "db" (d/db (ion/get-connection)))]
-    (->> (d/q
-          '[:find  ?text ?author
-            :where
-            [?e :fortune/author ?author]
-            [?e :fortune/text ?text]]
-          db)
-         vec
-         shuffle
-         first
-         (str/join " -"))))
+  [{:strs [db] :as context}]
+  (->> (d/q
+        '[:find  ?text ?author
+          :where
+          [?e :fortune/author ?author]
+          [?e :fortune/text ?text]]
+        db)
+       vec
+       shuffle
+       first
+       (str/join " -")))
 
 (defmethod respond {:tags #{"#fortune-teller"}}
-  [context]
-  (println "got to fortune-teller")
-  (println (pr-str context))
-  (let [db (get context "db" (d/db (ion/get-connection)))
-        Body (get context "Body")
-        teller-results (->> (some-body Body)
-                            (d/q
-                             '[:find ?text ?author-result
-                               :in $ ?author
-                               :where
-                               [?e :fortune/author ?author-result]
-                               [?e :fortune/text ?text]
-                               [(= ?match true)]
-                               [(iontalk.responder/author-match? $ ?e ?author) ?match]]
-                             db))
-        _ (println "Got Past teller-results:" (pr-str teller-results))
-        
-        result (first (shuffle (vec teller-results)))
-        _ (println "Got Past rand-nth:" (pr-str result))]
-    (str/join " -" result)))
+  [{:strs [db Body] :as context}]
+  (->> (some-body Body)
+       (d/q
+        '[:find ?text ?author-result
+          :in $ ?author
+          :where
+          [?e :fortune/author ?author-result]
+          [?e :fortune/text ?text]
+          [(= ?match true)]
+          [(iontalk.responder/author-match? $ ?e ?author) ?match]]
+        db)
+       vec
+       shuffle
+       first
+       (str/join " -")))
 
 (defmethod respond :default [context]
   (str "Sorry friend, my silly bot brain can't understand this big big world. Here is the context you sent me: " (pr-str context)))
